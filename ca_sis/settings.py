46730d37710888c53ec5bed95becd3dc62dcdebd
DwyxE <dwyxenrique@gmail.com>
2022-04-11 23:36:30 +0800

    a
    a

--- ca_sis/settings.py
+++ ca_sis/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
